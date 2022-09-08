232835f6e576f9622d3f832ada8b85c8e3b0cd99
Serva <servas230@gmail.com>
2022-09-08 14:21:42 +0200

    Added endpoint to change the status of the event attendees

--- routers/events.js
+++ routers/events.js
@@ -29,17 +29,6 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-// router.get("/", async (req, res, next) => {
-//   try {
-//     const eventResponse = await Event.findAll();
-//     console.log("eventResponse", eventResponse);
-//     res.send(eventResponse);
-//   } catch (e) {
-//     console.log("all events router error", e.message);
-//     next(e);
-//   }
-// });
-
 //
 
 //get details page for an event
@@ -62,19 +51,37 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
-// router.get("/:id", async (req, res, next) => {
-//   try {
-//     const { id } = req.params;
-
-//     const eventDetailsResponse = await Event.findByPk(id, {
-//       include: [User],
-//     });
-//     console.log("detail Response", eventDetailsResponse);
-//     res.send(eventDetailsResponse);
-//   } catch (e) {
-//     console.log("detail router error", e.message);
-//     next(e);
-//   }
-// });
+//   get token : http --ignore-stdin POST :4000/auth/login email="neda@neda.com" password="neda"
+// http --ignore-stdin PATCH :4000/events/4 status=false authorization:"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY2MjYzODg3NCwiZXhwIjoxNjYyNjQ2MDc0fQ.kQqnVyykAvw5OY-2_rTq-aJ-7qhnlqK-EScAURikKxA"
+// Get the userId from authMiddleware
+// http PATCH :4000/events/1  status=false
+router.patch("/:eventId", authMiddleware, async (req, res, next) => {
+  try {
+    const eventId = req.params.eventId;
+    const userId = req.user.id;
+    const { status } = req.body;
+    if (status === undefined) {
+      res.status(400).send("Please provide status");
+      return;
+    }
+    const attendee = await Attendee.findOne({
+      where: { eventId: eventId, userId: userId },
+    });
+    if (attendee) {
+      attendee.update({ status });
+      return res
+        .status(200)
+        .send({ message: "Status updated", attendee: attendee });
+    } else {
+      const newAttendee = await Attendee.create({ userId, eventId, status });
+      return res
+        .status(200)
+        .send({ message: "Status updated", attendee: newAttendee });
+    }
+  } catch (e) {
+    console.log("patch response", e.message);
+    next(e);
+  }
+});
 
 module.exports = router;
